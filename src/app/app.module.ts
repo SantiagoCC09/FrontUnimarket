5d8ca2ea461618190199b1d0c90bdca9d905dc10
SantiagoCC09 <santiago31800@hotmail.com>
2023-05-09 14:57:18 -0500

    Avance formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pagina/inicio/inicio.component';
 import { LoginComponent } from './pagina/login/login.component';
 import { RegistroComponent } from './pagina/registro/registro.component';
+import { CrearProductoComponent } from './pagina/crear-producto/crear-producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    CrearProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pagina/registro/registro.component.ts
+++ src/app/pagina/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UsuarioDTO } from 'src/app/modelo/usuario-dto';
+
+
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  usuario: UsuarioDTO;
 
+  constructor(){
+    this.usuario=new UsuarioDTO();
+  }
+
+  public registrar(){
+    console.log(this.usuario);
+  }
+  public sonIguales():boolean{
+    return this.usuario.password == this.usuario.confirmarPassword;
+  }
 }
