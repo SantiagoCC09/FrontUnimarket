dd90b0a1f25cdb5d1aa34003bcf6c9474c3c84eb
SantiagoCC09 <santiago31800@hotmail.com>
2023-05-10 22:57:40 -0500

    Clase producto implementada

---
+++ src/app/modelo/producto-dto.ts
@@ -0,0 +1,10 @@
+export class ProductoDTO {
+    nombre:string="";
+    descripcion:string="";
+    precio:number=0;
+    unidades:number=0;
+    imagenes:string[]=[];
+    categoria:string="";
+
+    
+}
--- src/app/pagina/crear-producto/crear-producto.component.ts
+++ src/app/pagina/crear-producto/crear-producto.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { ProductoDTO } from 'src/app/modelo/producto-dto';
 
 @Component({
   selector: 'app-crear-producto',
   templateUrl: './crear-producto.component.html',
   styleUrls: ['./crear-producto.component.css']
 })
-export class CrearProductoComponent {
 
+export class CrearProductoComponent {
+  categorias:string[];
+  producto: ProductoDTO;
+  archivos!:FileList;
+  
+
+  constructor(){
+    this.categorias=[];
+    this.cargarCategorias();
+    this.producto = new ProductoDTO();
+  }
+
+  private cargarCategorias(){
+    this.categorias.push('TECNOLOGIA');
+    this.categorias.push('DEPORTE');
+    this.categorias.push('VEHICULO');
+    this.categorias.push('ZAPATOS');
+  }
+  onFileChange(event:any){
+    if(event.target.files.length>0){
+      const files = event.target.files;
+      console.log(files);
+    }
+  }
+
+    public crearProducto(){
+      if(this.archivos != null && this.archivos.length>0){
+        console.log(this.producto);
+      }else{
+        console.log('Debe seleccionar al menos una imagen')
+      }
+    }
+
+  
 }
